7751e380be3e0c16dbab105f95d58be201e7fc18
Michael Reeves <mreeves@kabam.com>
2014-05-22 22:21:37 -0700

    Problem 33 solution

--- includes/classes/Problem33.php
+++ includes/classes/Problem33.php
@@ -26,9 +26,9 @@ class Problem33 extends Problem_Abstract
 
     /**
      * Description of input
-     * @const string INPUT
+     * @const string MAX_NUM_DEN
      */
-    const INPUT = '';
+    const MAX_NUM_DEN = 99;
 
     /**
      * Override default timeout of 60 seconds
@@ -50,18 +50,69 @@ class Problem33 extends Problem_Abstract
      */
     public function execute()
     {
-        return $this->findSomething(self::INPUT);
+        return $this->findCuriousFractionsProductDenominator(self::MAX_NUM_DEN);
     }
 
     /**
-     * Find "Something".
+     * Find the curious fractions (numerator and demoninator) as described above
      *
-     * @param string $number description
+     * @param int $upperLimit maximim numerator and denominator 
      *
-     * @return int description
+     * @return array $answers each element is an array where element 0 is numerator and element 1 is denominator
      */
-    private function findSomething($number){
+    private function findCuriousFractions($upperLimit){
+        $answers = array();
+        for ($num = 11; $num <= $upperLimit; $num++) {
+            for ($den = 11; $den <= $upperLimit; $den++) {
+                $result1 = $num/$den;
 
-        return;
+                $num_tmp = (string) $num;
+                $num1 = (int) $num_tmp{0};
+                $num2 = (int) $num_tmp{1};
+                $den_tmp = (string) $den;
+                $den1 = (int) $den_tmp{0};
+                $den2 = (int) $den_tmp{1};
+     
+                // result <= 1, denominator and numerator cannot be 0, nor the same as that would be "trivial"
+                if ($result1 <= 1 && $den2 != 0 & $num1 != 0 && $num2 == $den1 && $num1 != $num2) {
+                    $result2 = $num1/$den2;
+                    if (round($result2, 5) == round($result1,5)) {
+                        $answers[] = array($num1, $den2);
+                        echo $num."/".$den." and ".$num1."/".$den2."<br>\n"; // debug
+                    }
+                }
+                
+                // result <= 1, denominator and numerator cannot be 0, nor the same as that would be "trivial"
+                if ($result1 <= 1 && $den1 != 0 & $num2 != 0 && $num1 == $den2 && $num1 != $num2) {
+                    $result3 = $num2/$den1;
+                    if (round($result3, 5) == round($result1,5)) {
+                        $answers[] = array($num2, $den1);
+                        echo $num."/".$den." and ".$num2."/".$den1."<br>\n"; // debug
+                    }
+                }
+            } 
+        }
+        return $answers;
+    }
+
+
+    /**
+     * Find the denominator of all the products of the curious fractions once the numerator has been reduced
+     *
+     * @param int $upperLimit maximim numerator and denominator 
+     *
+     * @return int denominator of the product of the curious fractions
+     */
+    private function findCuriousFractionsProductDenominator($upperLimit){
+        $answers = $this->findCuriousFractions($upperLimit);
+        $product = 1;
+        foreach ($answers as $a) {
+            $num = $a[0];
+            $den = $a[1];
+            $product *= $num;
+            $product /= $den;
+        }
+        $result = 1/$product; // This would not work of course if the numerator did not reduce to 1. Little cheat shortcut! 
+        return $result;
     }
 }
