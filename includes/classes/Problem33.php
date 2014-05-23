8744a7c4d08300c9fdd150eed5129a07cf682ba2
Michael Reeves <mreeves@kabam.com>
2014-05-22 20:12:00 -0700

    Initial commit for Problem 33

---
+++ includes/classes/Problem33.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Project Euler - Problem 33
+ *
+ * Digit canceling fractions
+ *
+ * The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+ * We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+ * There are exactly four non-trivial examples of this type of fraction, less than one in value, and containing two digits in the numerator and denominator.
+ * If the product of these four fractions is given in its lowest common terms, find the value of the denominator. 
+ *
+ * @category ProjectEuler
+ * @package Problem33
+ * @author Michael Reeves <mike.reeves@gmail.com>
+ * @link http://projecteuler.net/problem=33
+ *
+ */
+class Problem33 extends Problem_Abstract
+{
+
+    /**
+     * Project Euler says each problem should take no more than 1 minute. If your computer is slow make this larger.
+     * $const int PROBLEM_TIMEOUT Used with set_timeout_limit to throw a timeout if problem computation takes too long.
+     */
+    const PROBLEM_TIMEOUT_OVERRIDE = 60;
+
+    /**
+     * Description of input
+     * @const string INPUT
+     */
+    const INPUT = '';
+
+    /**
+     * Override default timeout of 60 seconds
+     */
+    public function __construct()
+    {
+        parent::__construct(); 
+        set_time_limit(self::PROBLEM_TIMEOUT_OVERRIDE);
+        if (!extension_loaded('bcmath')) {
+            // Placeholder for any extensions required for this problem's code
+            // die('BCMath extension required. See http://www.php.net/manual/en/book.bc.php .');
+        }
+    }
+
+    /**
+     * Wrapper method to output our answer with the appropriate input variables
+     *
+     * @return int
+     */
+    public function execute()
+    {
+        return $this->findSomething(self::INPUT);
+    }
+
+    /**
+     * Find "Something".
+     *
+     * @param string $number description
+     *
+     * @return int description
+     */
+    private function findSomething($number){
+
+        return;
+    }
+}
