3f9407e4b6637b83c15aee2a8c5e187166ae53ba
Michael Reeves <mreeves@kabam.com>
2014-12-05 19:43:12 -0800

    Solved Problem 46

--- includes/classes/Problem46.php
+++ includes/classes/Problem46.php
@@ -24,26 +24,10 @@
 class Problem46 extends Problem_Abstract
 {
     /**
-     * Project Euler says each problem should take no more than 1 minute. 
-     * If your computer is slow make this larger.
-     * @const int TIMEOUT_OVERRIDE Used with an override method to control how long it 
-     * takes for the script to timeout
-     */
-    const TIMEOUT_OVERRIDE = 60;
-
-    /**
-     * Project Euler is silent on space complexity. PHP uses a LOT of memory for arrays. 
-     * Something like 20x what you would expect. 
-     * @const int MEMORY_OVERRIDE Used with an override method to control how much memory
-     * the script is allowed to consume.
-     */
-    const MEMORY_OVERRIDE = '64M';
-
-    /**
      * Description of input
      * @const string INPUT
      */
-    const INPUT = '';
+    const UPPER_BOUND = 100000;
 
     /**
      * Override default timeout of 60 seconds
@@ -51,13 +35,6 @@ class Problem46 extends Problem_Abstract
     public function __construct()
     {
         parent::__construct(); 
-
-        // $this->overrideTimeoutAndMemoryLimit(self::TIMEOUT_OVERRIDE, self::MEMORY_OVERRIDE);
-
-        if (!extension_loaded('bcmath')) {
-            // Placeholder for any extensions required for this problem's code
-            // die('BCMath extension required. See http://www.php.net/manual/en/book.bc.php .');
-        }
     }
 
     /**
@@ -67,18 +44,80 @@ class Problem46 extends Problem_Abstract
      */
     public function execute()
     {
-        return $this->findSomething(self::INPUT);
+        return $this->testGoldbachsOtherConjecture(self::UPPER_BOUND);
     }
 
     /**
-     * Find "Something".
+     * Test Goldbach's other conjecture where we are testing all numbers from 3 to $upper_bound
+     * to find a number x where x != (prime # + 2 * y^2) where y is an integer and x is not itself prime 
      *
-     * @param string $number description
+     * @param string $upper_bound Final number to test
      *
-     * @return int description
+     * @return int The first number which does not fit these constraints
      */
-    private function findSomething($number){
-
-        return;
+    private function testGoldbachsOtherConjecture($upper_bound){
+        $answer = 0;
+        for ($i = 3; $i < $upper_bound; $i = $i+2) {        
+            if (! $this->isPrime($i)) {
+                // echo "\nTesting $i to see if it fits Goldbach's other conjecture\n"; // debug
+                $found = false;
+                for ($p = 3; $p < $i; $p = $p+2) {   
+                    if (! $this->isPrime($p)) { continue; }
+                    $tw_sq = $i - $p;
+                    if ($tw_sq < 2) { break; }
+                    $sq = $tw_sq / 2;
+                    // echo "prime is $p, sqr_rt is ".sqrt($sq)."\n"; // debug
+                    if (($sq == floor($sq)) && (sqrt($sq) == floor(sqrt($sq)))) {
+                        // echo "For $i, prime is $p and square root is $sq\n"; // debug
+                        $found = true;
+                        break;
+                    } 
+                }
+                if (!$found) {
+                    $answer = $i;
+                    // echo "$i is an odd composite that does not fit the conjecture\n"; // debug
+                    break;
+                }
+            }
+        }
+        return ($answer > 0) ? $answer : "not found";
     }
+
+    /**
+     * Test for Prime-ness
+     *
+     * @param string $n Number to test for primality
+     *
+     * @return boolean Is number prime?
+     */
+    private function isPrime($n) {
+        static $primes = array(2, 3);
+
+        if ($n === 1) {
+            return false;
+        } elseif ($n <= 3) {
+            return true;
+        } elseif ($n % 2 == 0 || $n % 3 == 0) {
+            return false;
+        } else {
+            foreach ($primes as $prime) { // Use sieve
+                if ($n > $prime && $n % $prime == 0) {
+                    return false;
+                }
+            }
+            // TODO: Come back and understand this prime test algo better
+            for ($i = 5; $i <= sqrt($n) + 1; $i += 6) {
+                if ($n % $i == 0 || $n % ($i + 2) == 0) {
+                    return false;
+                }
+            }
+            // Store in sieve
+            if ($n < 300 && !in_array($n, $primes)) {
+                $primes[] = $n;
+            }
+            return true;
+        }
+    }
+
+
 }
