32ae86e23c373c9231e29ed6016dc429066c681c
Michael Reeves <mike.reeves@gmail.com>
2015-11-29 01:28:14 -0800

    Solved Problem 47

--- includes/classes/Problem47.php
+++ includes/classes/Problem47.php
@@ -27,7 +27,7 @@ class Problem47 extends Problem_Abstract
      * @const int TIMEOUT_OVERRIDE Used with an override method to control how long it 
      * takes for the script to timeout
      */
-    const TIMEOUT_OVERRIDE = 60;
+    const TIMEOUT_OVERRIDE = 240;
 
     /**
      * Project Euler is silent on space complexity. PHP uses a LOT of memory for arrays. 
@@ -38,53 +38,90 @@ class Problem47 extends Problem_Abstract
     const MEMORY_OVERRIDE = '64M';
 
     /**
-     * Description of input
-     * @const string INPUT
+     * Input value
+     * @const int INPUT
      */
-    const INPUT = '';
+
+    // const INPUT = 2; // Test 1: Answer should be 14
+    // const INPUT = 3; // Test 2: Answer should be 644
+    const INPUT = 4;
 
     /**
      * Override default timeout of 60 seconds
      */
     public function __construct()
     {
-        parent::__construct(); 
-
-        // $this->overrideTimeoutAndMemoryLimit(self::TIMEOUT_OVERRIDE, self::MEMORY_OVERRIDE);
-
-        if (!extension_loaded('bcmath')) {
-            // Placeholder for any extensions required for this problem's code
-            // die('BCMath extension required. See http://www.php.net/manual/en/book.bc.php .');
-        }
+        parent::__construct();
+        $this->overrideTimeoutAndMemoryLimit(self::TIMEOUT_OVERRIDE, self::MEMORY_OVERRIDE);
     }
 
     /**
      * Wrapper method to output our answer with the appropriate input variables
-     *
      * @return int
      */
     public function execute()
     {
-        return $this->findSomething(self::INPUT);
+        return $this->findConsecutiveNumbersWithNDistinctPrimeFactors(self::INPUT);
     }
 
     /**
-     * Find "Something".
+     * Find $n consecutive numbers with $n distinct prime factors.
      *
-     * @param string $number description
-     *
-     * @return int description
+     * @param int $n Number of consecutive integers with same number of unique prime factors
+     * @return int First integer in set of consecutive numbers
      */
-    private function findSomething($number){
+    private function findConsecutiveNumbersWithNDistinctPrimeFactors($n){
 
-        return;
+        // Pre-populate primes
+        $primes = [];
+        for ($i = 2; $i < 200000; $i++) {
+            if ($this->isPrime($i)) {
+                $primes[$i] = $i;
+            }
+        }
+
+        $sequence = [];
+        for ($j = 2; $j < 200000; $j++) {
+            if (isset($primes[$j])) {
+                // echo "skipping prime $j <br>\n"; // debug
+                $sequence = [];
+                continue;
+            }
+            $factors = [];
+            $test = $j;
+            while ($test > 1) {
+                foreach ($primes as $p) {
+                    if ($test % $p == 0) {
+                        if (!in_array($p, $factors)) {
+                            $factors[] = $p;
+                        }
+                        $test = $test / $p;
+                        if (count($factors) > $n) {
+                            // echo "$j has too many factors<br>\n"; // debug
+                            break 2;
+                        } elseif ($test == 1) {
+                            if (count($factors) == $n) {
+                                $sequence[] = $j;
+                                if (count($sequence) == $n) {
+                                    // echo "found numbers: ".implode($sequence, ', ')."<br>\n"; // debug
+                                    return $sequence[0];
+                                }
+                            } else { // reset
+                                $sequence = [];
+                            }
+                            break 2;
+                        }
+                    }
+                }
+            }
+            // echo "$j has factors of ".implode(", ", $factors)."<br>\n"; // debug
+        }
     }
 
     /**
-     * Test for Prime-ness
+     * Test for Prime-ness (cribbed from problem 50)
      *
      * @param string $n Number to test for primality
-     *
      * @return boolean Is number prime?
      */
     private function isPrime($n) {
